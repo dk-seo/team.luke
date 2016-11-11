cfeb4df9d122e3399c5abae46956bc54b6ca110d
DeokHwa <jeromed1992@gmail.com>
2016-11-11 12:51:49 -0800

    Declared a recommender function
    Will start the work

--- EngineLuke/EngineLuke/main.cpp
+++ EngineLuke/EngineLuke/main.cpp
@@ -3,19 +3,6 @@
 
 int main()
 {
-	// debug output stream that will be printed detailed process of 
-	// recommendation system
-	std::ofstream debug_out("Out/movieusers_debug.txt", std::ios::out);
-
-	// final table output stream
-	std::ofstream out("Out/movieusers_out.csv", std::ios::out);
-
-	// test recommender system using movie data.
-	RecommenderSystemTest_Movies(
-		"Data/movie data.csv",
-		"Data/movie data_test.csv",
-		&debug_out, 
-		&out);
 
 	return 0;
 }
